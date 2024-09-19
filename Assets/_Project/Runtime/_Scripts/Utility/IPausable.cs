16bc24abdfe150e646b693302a514230151e7eb8
Lum [School] <itsluminas@gmail.com>
2024-09-19 14:08:20 +0200

    Thursday.

--- Assets/_Project/Runtime/_Scripts/Utility/IPausable.cs
+++ Assets/_Project/Runtime/_Scripts/Utility/IPausable.cs
@@ -1,8 +1,8 @@
-public interface IDisableable
+public interface IPausable
 {
     /// <summary>
     /// Toggles the disable state of the object.
     /// </summary>
     /// <returns></returns>
-    public void Toggle();
+    public void Pause();
 }
